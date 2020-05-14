59d9e112de618151c8c8af09d92d0e70e2038af6
Michelle Horn <michellehorn@live.com>
2020-05-14 09:59:16 -0300

    changes on challenge

--- src/main.js
+++ src/main.js
@@ -1,24 +1,58 @@
-// Abaixo está a lógica da nossa calculadora mas perceba que tem muita coisa faltando,
-// e é com você mudar isso para nós termos uma calculadora funcional.
-// Ao final disso, mude também o css, o html, adicione imagens se desejar, essa calculadora é
-// deixe essa calculadora com a sua cara
+const botao = document.querySelector("#botaoCalcular")
+botao.addEventListener("click", calcularChurrasco)
 
 const calcularChurrasco = () => {
-    const quantidadePessoas = pessoas.value;
-    let quantidadeCarne = 0
-    //Aqui você irá inserir a lógica para calcular
-    //a quantidade de carne, acompanhamentos e cerveja para o churrasco.
-    //perceba que faltam os campos de cerveja e acompanhamentos, isso fica por sua conta :)
-    resultadoCarne.innerHTML = 100;
+
+    // Coleta valor dos inputs
+    const mulheres = document.getElementById('mulheres-qnt').value;
+    const homens = document.getElementById('homens-qnt').value;
+    const criancas = document.getElementById('criancas-qnt').value;
+
+    const acompanhamentos = document.getElementById('acompanhamentos').value;
+    const bebidasAlcoolicas = document.getElementById('bebidas-alcoolicas').value;
+    const bebidasNaoAlcoolicas = document.getElementById('bebidas-nao-alcoolicas').value;
+
+
+    // Logica de valores por grama e multiplicacao da quantidade de pessoas
+    let totalCarne = mulheres * 150 + homens * 200 + criancas * 100
+
+    // Soma da quantidade de pessoas
+    const totalPessoas = Number(mulheres) + Number(homens) + Number(criancas)
+    
+    // 50g de acompanhamento por pessoa
+    const totalAcompanhamento = acompanhamentos ? 50 * Number(totalPessoas) : 0
+    
+    // 400ml de bebida nao alcoolica para cada pessoa
+    const totalBebidasNaoAlcoolicas = bebidasNaoAlcoolicas ? 500 * Number(bebidasNaoAlcoolicas) : 0
+    
+    // 500ml de bebida alcoolica para cada pessoa :'D
+    const totalBebidasAlcoolicas = bebidasAlcoolicas ? 500 * Number(bebidasAlcoolicas) : 0
+
+    
+
+    document.getElementById("total-carne").innerHTML = `${getValueMeasure(totalCarne, 1)}`
+    document.getElementById("total-pessoas").innerHTML = `${totalPessoas} pessoas`
+    document.getElementById("total-acompanhamento").innerHTML = `${getValueMeasure(totalAcompanhamento, 1)}`
+    document.getElementById("total-bebidas-nao-alcoolicas").innerHTML = `${getValueMeasure(totalBebidasNaoAlcoolicas, 2)}`
+    document.getElementById("total-bebidas-alcoolicas").innerHTML = `${getValueMeasure(totalBebidasAlcoolicas, 2)}`
+
 }
-const botao = document.querySelector("#botaoCalcular").addEventListener("click", calcularChurrasco)
-
-const pessoas = document.querySelector('#pessoas-qnt')
-//insira aqui a lógica para pegar também a quantidade de pessoas vegetarianas
 
 
-const resultadoCarne = document.querySelector('#carne-qnt')
-//insira aqui a lógica para pegar os campos de resultado dos acompanhamentos e das cervejas
+/* Definição da medida utilizada a partir da quantidade medida em gramas
+type 1: comidas
+type 2: bebidas
+*/
 
-
-botaoCalcular.addEventListener("click", calcularChurrasco)
+function getValueMeasure(val, type) {
+    switch(type) {
+        case 1: {
+            return val >= 1000 ? `${val/1000}kg` : `${val}g`
+            break;
+        }
+        case 2: {
+            return val >= 1000 ? `${val/1000}L` : `${val}ml`
+            break;
+        }
+    }
+}
